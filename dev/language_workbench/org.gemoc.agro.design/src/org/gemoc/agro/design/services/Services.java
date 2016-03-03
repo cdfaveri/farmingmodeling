4ee108fa9b85987950b6d3d08dc309fb1d978e77
cbrun <cedric.brun@obeo.fr>
2016-03-03 17:35:25 +0100

    Dblc click now open the text editor

--- dev/language_workbench/org.gemoc.agro.design/src/org/gemoc/agro/design/services/Services.java
+++ dev/language_workbench/org.gemoc.agro.design/src/org/gemoc/agro/design/services/Services.java
@@ -2,11 +2,23 @@ package org.gemoc.agro.design.services;
 
 import java.util.Set;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EStructuralFeature.Setting;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.sirius.business.api.session.Session;
 import org.eclipse.sirius.business.api.session.SessionManager;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.ide.IDE;
+import org.eclipse.ui.texteditor.AbstractTextEditor;
+import org.eclipse.xtext.nodemodel.ICompositeNode;
+import org.eclipse.xtext.nodemodel.util.NodeModelUtils;
+import org.eclipse.xtext.resource.XtextResource;
 import org.gemoc.agro.exploitation.Surface;
 import org.gemoc.agro.scientific.ExploitationAnalysis;
 import org.gemoc.agro.scientific.ScientificPackage;
@@ -163,6 +175,37 @@ public class Services {
       return res.getURI().lastSegment();
     }
     return "";
+  }
+
+  public EObject openTextEditor(EObject any) {
+    if (any != null && any.eResource() instanceof XtextResource
+        && any.eResource().getURI() != null) {
+
+      String fileURI = any.eResource().getURI().toPlatformString(true);
+      IFile workspaceFile = ResourcesPlugin.getWorkspace().getRoot()
+          .getFile(new Path(fileURI));
+      if (workspaceFile != null) {
+        IWorkbenchPage page = PlatformUI.getWorkbench()
+            .getActiveWorkbenchWindow().getActivePage();
+        try {
+          IEditorPart openEditor = IDE.openEditor(page, workspaceFile,
+              "org.gemoc.agro.ActivitiesDSL", true);
+          if (openEditor instanceof AbstractTextEditor) {
+            ICompositeNode node = NodeModelUtils.findActualNodeFor(any);
+            if (node != null) {
+              int offset = node.getOffset();
+              int length = node.getTotalEndOffset() - offset;
+              ((AbstractTextEditor) openEditor).selectAndReveal(offset, length);
+            }
+          }
+          // editorInput.
+        } catch (PartInitException e) {
+          // Put your exception handler here if you wish to.
+        }
+      }
+    }
+    System.out.println(any);
+    return any;
   }
 
 }
